11aa4a995dbe0edbfa30130f2c7bca8ce3371c41
Salman Akram <thunder9ruby@gmail.com>
2015-09-01 20:46:13 -0400

    adding more partials and images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.sticky
+//= require parallax.min.js
+//= require main
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".header").sticky();
+});
+
+$('.parallax-window').parallax({
+  data-image-src=<%= asset_path "122H.jpg" %>
+});
