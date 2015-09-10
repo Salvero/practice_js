c257dc899cfdcbe4f4bb8dd37c206b503974f957
Salman Akram <thunder9ruby@gmail.com>
2015-09-10 17:33:07 -0400

    assets precompiled locally

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,13 +5,18 @@ $(document).ready(function(){
 });
 
 // Parallax
-$('.parallax-window').parallax({
-  data-image-src=<%= asset_path "204H.jpg" %>
+$(document).ready(function(){
+  $('.parallax-window').parallax({
+    data-image-src=<%= asset_path "204H.jpg" %>
+  });
 });
 
-$('.parallax-window2').parallax({
-  data-image-src=<%= asset_path "machine.jpg" %>
+$(document).ready(function(){
+  $('.parallax-window2').parallax({
+    data-image-src=<%= asset_path "machine.jpg" %>
+  });
 });
+
 
 // Simple Lightbox
 
