32b1af8442c1a1fa37e52d34d764158ad60a6876
Salman Akram <thunder9ruby@gmail.com>
2015-09-10 19:31:26 -0400

    remove some jquery section

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,22 +3,3 @@
 $(document).ready(function(){
   $(".header").sticky();
 });
-
-// Parallax
-// $('.parallax-window').parallax({
-//   data-image-src=<%= asset_path "204H.jpg" %>
-// });
-//
-// $('.parallax-window2').parallax({
-//   data-image-src=<%= asset_path "machine.jpg" %>
-// });
-
-
-
-// Simple Lightbox
-
-// $('.thumbnail').simpleLightbox(options);
-
-// var gallery = $('.thumbnail img').simpleLightbox();
-//
-// gallery.next();
