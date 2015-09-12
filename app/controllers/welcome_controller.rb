80c1f94b21391f873f94795370cb061d9b329729
Salman Akram <thunder9ruby@gmail.com>
2015-09-11 20:13:33 -0400

    move files to config

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  def robots
+    robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
+    render :text => robots, :layout => false, :content_type => "text/plain"
+  end
+
   def index
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  
+  get '/robots.txt' => 'welcome#robots'
+
   root 'welcome#index'
 
 end
